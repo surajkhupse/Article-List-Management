bf3078d7c93b7f42de908f2c6718fef4104e09ed
Suraj Khupse <surajkhupse22@gmail.com>
2020-05-28 19:42:34 +0530

    added footer line

--- application/views/Admin/Footer.php
+++ application/views/Admin/Footer.php
@@ -1,6 +1,7 @@
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> -->
 <script src="<?= base_url("Assets/js/bootstrap.js")?>"></script>
+<p>This site created by suraj"</p>
 
 </body>
 </html>
